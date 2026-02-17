5bb320bfdde996a334771f08feb470d3f820fae5
PawelKrawczyk96 <pawel.krawczyk@interia.com>
2026-02-17 16:20:31 +0100

    add panel statistics card, .gitignore, folder with icons

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,5 +1,20 @@
 import "./style.css";
+import { StatisticsCard } from "@components/Cards";
+import { ReactComponent as BookIcon } from "icons/book.svg";
 
 export const HomePage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <StatisticsCard
+        statisticNumber={156}
+        statisticDescription={{
+          description: "+12 w tym tyg.",
+          isPositive: true,
+        }}
+        icon={<BookIcon />}
+        iconColor="red"
+        title={"Total fiszek"}
+      />
+    </div>
+  );
 };
